0ed63532a5bc3fff6fe5bc1cdb2115ba7602337b
Etienne Champagne <etienne.champagne.ec@gmail.com>
2020-03-01 11:51:28 +0100

    orbit wip

--- Base9/Assets/Scripts/CameraOrbit.cs
+++ Base9/Assets/Scripts/CameraOrbit.cs
@@ -5,6 +5,10 @@ using UnityEngine.EventSystems;
 
 public class CameraOrbit : MonoBehaviour
 {
+    [Header("External Ref")]
+    [SerializeField]
+    private Transform focusPoint;
+
     [SerializeField]
     private float maxAngle;
 
@@ -12,20 +16,27 @@ public class CameraOrbit : MonoBehaviour
     private float height;
 
     [SerializeField]
-    private Transform focusPoint;
+    private float speed;
+
+    [SerializeField]
+    private float damp;
+
 
     private bool bOrbiting;
+    private Vector3 velocity;
+    private Vector3 rotation;
 
     // Start is called before the first frame update
     void Start()
     {
         transform.position = focusPoint.position + new Vector3(0, height, 0);
+        rotation = transform.rotation.eulerAngles;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if((Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began))
+        if ((Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began))
         {
             if (EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId))
             {
@@ -33,8 +44,7 @@ public class CameraOrbit : MonoBehaviour
                 Debug.Log("Touch orbiting..");
             }
         }
-
-        if ((Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended) && bOrbiting)
+        else if ((Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended) && bOrbiting)
         {
             bOrbiting = false;
             Debug.Log("Touch orbiting ended.");
@@ -49,10 +59,53 @@ public class CameraOrbit : MonoBehaviour
                 Debug.Log("Click orbiting...");
             }
         }
-        if (Input.GetMouseButtonUp(0) && bOrbiting)
+        else if (Input.GetMouseButtonUp(0) && bOrbiting)
         {
             bOrbiting = false;
             Debug.Log("Click orbiting ended.");
         }
+    }
+
+    /*
+     * void LateUpdate()
+    {
+        // get velocity from mouse movement
+        if (bOrbiting)
+        {
+            velocity.x += speed * Input.GetAxis("Mouse X");
+            velocity.y += speed * Input.GetAxis("Mouse Y");
+        }
+
+        // rotate camera
+        transform.RotateAround(focusPoint.position, Vector3.up, velocity.x);
+        transform.RotateAround(focusPoint.position, Vector3.right, velocity.y);
+
+        Quaternion quat = transform.rotation;
+        Vector3 euler = transform.rotation.eulerAngles;
+        if (euler.x < 90 - maxAngle)
+            euler.x = 90 - maxAngle;
+        else if (euler.x > 90 + maxAngle)
+            euler.x = 90 + maxAngle;
+
+        if (euler.y > maxAngle)
+            euler.y = maxAngle;
+        else if (euler.y < -maxAngle)
+            euler.y = -maxAngle;
+
+        quat.eulerAngles = euler;
+        transform.rotation = quat;
+        
+        // damping
+        velocity.x = Mathf.Lerp(velocity.x, 0, Time.deltaTime * damp);
+        velocity.y = Mathf.Lerp(velocity.y, 0, Time.deltaTime * damp);
+    }
+    */
+    public static float ClampAngle(float angle, float min, float max)
+    {
+        if (angle < -360F)
+            angle += 360F;
+        if (angle > 360F)
+            angle -= 360F;
+        return Mathf.Clamp(angle, min, max);
     }
 }
